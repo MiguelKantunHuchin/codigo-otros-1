4b38fdef2f8c178593a2628e5e08a34d5e7c637d
Miguel Kantun <miguelkh21@gmail.com>
2023-06-21 20:02:37 -0600

    Se arreglaron los errores en HTML, CSS y JS. El programa ya funciona

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog'); //Se cambia el selector de ID por un selector de clase
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //console.log(data); esta linea por ahora no es necesario al no estar inicializada la variable
+
+
+  //Creamos una variable para almacenar el response en un JSON
+
+  let data = await response.json();
+
+
+  //Se cambiaron las plantillas literales a simplemente acceder a la info del objeto data, la plantilla innecesaria ya que no se concatenan datos
+  $n.textContent = data.name;
+  $b.textContent = data.blog;
+  $l.textContent = data.location;
 }
 
 function handleError(err) {
